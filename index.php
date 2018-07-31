3092b68a40a79b6bb6d5237b5cbdbeaa79f6becb
Katlego Kegopotswe <katlegokeg@gmail.com>
2018-07-31 20:16:44 +0200

    Added functions and sidebar

--- index.php
+++ index.php
@@ -5,6 +5,11 @@
 	<?php wp_header(); ?>
 </head>
 <body <?php body_class(); ?>>
+<nav>
+	<ul class="nav navbar-nav navbar-right">
+		<li></li>
+	</ul>
+</nav>
 	<?php if(have_posts()): ?>
     <?php while(have_posts()):the_post() ?>
  		<h2><?php the_title(); ?></h2>
